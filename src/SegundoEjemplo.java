93687490179d4b442eaa7bd292d7b4046258c958
ronal2003 <107308574+ronal2003@users.noreply.github.com>
2022-07-02 08:32:09 -0500

    segunda subida de continuaciondesiclos

---
+++ src/SegundoEjemplo.java
@@ -0,0 +1,14 @@
+public class SegundoEjemplo {
+    public static void main(String[] args) {
+        //describe los numerous del 10 al 1
+
+        int numero = 10;
+
+        while(numero >= 1)
+        {
+            System.out.println("numero " + numero);
+
+            numero = numero -1;
+        }
+    }
+}
--- src/ciclos.java
+++ src/ciclos.java
@@ -1,5 +1,20 @@
 public class ciclos {
     public static void main(String[] args) {
+    //ciclo while
+    //pintar los numeros del 1 al 10
 
+        boolean noHaterminado = true;
+
+        int numero = 1;
+       while(noHaterminado) {
+          System.out.println("Numeros " + numero);
+          numero = numero + 1;
+          if (numero > 10)
+          {
+              noHaterminado = false;
+
+          }
+
+      }
     }
 }
