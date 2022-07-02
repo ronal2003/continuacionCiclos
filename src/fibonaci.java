227249f507aa7102c2455301118f75e30bb042d7
ronal2003 <107308574+ronal2003@users.noreply.github.com>
2022-07-02 08:37:25 -0500

    tercer subida fibonaci

---
+++ src/fibonaci.java
@@ -0,0 +1,15 @@
+public class fibonaci {
+    public static void main(String[] args) {
+        int i = 0, a = 0, b = 1, c = 0;
+        for (i = 0; i < 10; i++) {
+            System.out.print(a + ",");
+            if (i < 9) {
+                c = a + b;
+                a = b;
+                b = c;
+            } else {
+                System.out.print(a);
+            }
+        }
+    }
+}
